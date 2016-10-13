47891ac5c5c0ae72decff90219579965307b0e02
cecilesauder <cecile.sauder@gmail.com>
2016-10-13 17:54:39 +0200

    change in the output texte format

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -17,7 +17,8 @@ shinyUI(fluidPage(
   sidebarPanel(
     #actionButton("new_game", label = "New game"),
     actionButton("action", label = "SET !"),
-    verbatimTextOutput("sel_cards")
+    textOutput("sel_cards")
+    
   ),
   
   # Show a plot of the generated distribution
